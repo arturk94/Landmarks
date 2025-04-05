88cdad3db9cc16f889cd38e9d6e027ba12e4135e
Artur KNOTHE <artur@knothe.org>
2025-04-05 21:07:30 +0200

    Adding Landmarks

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Artur KNOTHE on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
